cbdf2d47e3806071ac979ad2c82f595c8a40f9aa
xxNagibatorx1337x <109899654+xxNagibatorx1337x@users.noreply.github.com>
2023-05-02 18:38:01 +0300

    Create main.php

---
+++ webtest/main.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!empty($_SERVER["HTTP_CLIENT_IP"]))
+{
+  $IP = $_SERVER["HTTP_CLIENT_IP"];
+}
+else if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+{
+ $IP = $_SERVER["HTTP_X_FORWARDED_FOR"];  
+}
+else
+{
+  $IP = $_SERVER["REMOTE_ADDR"];  
+}
+echo $IP;
