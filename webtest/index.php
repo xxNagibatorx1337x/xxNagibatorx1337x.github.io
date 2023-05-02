1ef0064301778a87f8117d67729c30486ffc0de7
xxNagibatorx1337x <109899654+xxNagibatorx1337x@users.noreply.github.com>
2023-05-02 18:24:13 +0300

    Update index.php

--- webtest/index.php
+++ webtest/index.php
@@ -1,14 +1,1 @@
-<?php
-if (!empty($_SERVER["HTTP_CLIENT_IP"]))
-{
-  $IP = $_SERVER["HTTP_CLIENT_IP"];
-}
-else if (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
-{
- $IP = $_SERVER["HTTP_X_FORWARDED_FOR"];  
-}
-else
-{
-  $IP = $_SERVER["REMOTE_ADDR"];  
-}
-echo $IP;
+echo "ht";
